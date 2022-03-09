7cb5a4a7dcb5f657a597b4978e8b3f73fe544afc
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2022-03-08 20:36:36 -0700

    Initial Commit

---
+++ IntroToObjectiveC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IntroToObjectiveC
+//
+//  Created by Mitch Andrade on 3/8/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
